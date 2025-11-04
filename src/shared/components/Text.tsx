68b6c56cdb7abd23460a2dde9c82abe8b245ba58
obb8923 <obb8923@naver.com>
2025-11-04 17:35:15 +0900

    Text 컴포넌트에서 fontweight조절 스타일 삭제

--- src/shared/components/Text.tsx
+++ src/shared/components/Text.tsx
@@ -1,41 +1,41 @@
 import {Text as RNText, TextStyle} from 'react-native';
 export type TextProps = {
     text: string;
-    type: 'regular' | 'semibold' | 'extrabold' | 'black';
+    // type: 'regular' | 'semibold' | 'extrabold' | 'black';
     className?: string;
     style?: TextStyle | TextStyle[];
     numberOfLines?: number;
   };
-const fontStyle = (type: 'regular' | 'semibold' | 'extrabold' | 'black'): TextStyle => {
-  switch(type){
-    case 'regular':
-      return {
-        fontFamily: 'MemomentKkukkukkR',
-      };
-    case 'semibold':
-      return {
-        fontFamily: 'MemomentKkukkukkR',
-      };
-    case 'extrabold':
-      return {
-        fontFamily: 'MemomentKkukkukkR',
-      };
-    case 'black':
-      return {
-        fontFamily: 'MemomentKkukkukkR',
-      };
-    default:
-      return {
-        fontFamily: 'MemomentKkukkukkR',
-      };
-  }
-}
-export const Text = ({text, type='regular', ...props}: TextProps) => {
+// const fontStyle = (type: 'regular' | 'semibold' | 'extrabold' | 'black'): TextStyle => {
+//   switch(type){
+//     case 'regular':
+//       return {
+//         fontFamily: 'MemomentKkukkukkR',
+//       };
+//     case 'semibold':
+//       return {
+//         fontFamily: 'MemomentKkukkukkR',
+//       };
+//     case 'extrabold':
+//       return {
+//         fontFamily: 'MemomentKkukkukkR',
+//       };
+//     case 'black':
+//       return {
+//         fontFamily: 'MemomentKkukkukkR',
+//       };
+//     default:
+//       return {
+//         fontFamily: 'MemomentKkukkukkR',
+//       };
+//   }
+// }
+export const Text = ({text, ...props}: TextProps) => {
   return (
     <RNText 
     {...props}
       className={props.className}
-      style={[fontStyle(type), props.style]}
+      style={[{fontFamily: 'MemomentKkukkukkR'}, props.style]}
       numberOfLines={props.numberOfLines}>
       {text}
     </RNText>
