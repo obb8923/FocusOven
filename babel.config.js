7cecae1db0b1d29fe0f95d808ed3c6a5047fcc8b
obb8923 <obb8923@naver.com>
2025-11-04 16:28:22 +0900

    1

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:@react-native/babel-preset','nativewind/babel'],
+  plugins: [
+    'react-native-worklets/plugin'// 무조건 마지막에 추가
+  ],
 };
